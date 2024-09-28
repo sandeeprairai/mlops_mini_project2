bf17d8f8464b83d6f5de4bbe3f91a11694292b62
sandeeprairai <sandeeprairai339@gmail.com>
2024-09-28 11:15:59 +0530

    add dagshub token

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -10,15 +10,28 @@ import mlflow
 import mlflow.sklearn
 import dagshub
 import os
-
-
-
 import dagshub
 
+import os
+
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "sandeeprairai"
+repo_name = "mlops_mini_project2"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 
-mlflow.set_tracking_uri('https://dagshub.com/sandeeprairai/mlops_mini_project2.mlflow')
-dagshub.init(repo_owner='sandeeprairai', repo_name='mlops_mini_project2', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/sandeeprairai/mlops_mini_project2.mlflow')
+# dagshub.init(repo_owner='sandeeprairai', repo_name='mlops_mini_project2', mlflow=True)
 
 
 
